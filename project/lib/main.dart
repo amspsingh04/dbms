d540c09e21755f98d753e7639199deb43a7fe4f1
amspsingh04 <sp.singh.150604@gmail.com>
2024-04-14 23:11:54 +0530

    .

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'send.dart';
 
 void main() {
-  (const MyApp());
+  const MyApp();
 }
 
 class MyApp extends StatelessWidget {
@@ -9,29 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: UserListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Welcome to my app!'),
-      ),
-    );
-  }
-}
 //https://45bd-115-240-194-54.ngrok-free.app/
--- project/lib/send.dart
+++ project/lib/send.dart
@@ -1,13 +1,53 @@
-// ignore: depend_on_referenced_packages
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void fetchData() async {
-  var url = Uri.parse('https://45bd-115-240-194-54.ngrok-free.app/');
-  var response = await http.get(url);
+class UserListScreen extends StatefulWidget {
+  const UserListScreen({super.key});
 
-  if (response.statusCode == 200) {
-    print('Response data: ${response.body}');
-  } else {
-    print('Request failed with status: ${response.statusCode}.');
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  Future<List<dynamic>> fetchUsers() async {
+    var response =
+        await http.get(Uri.parse('http://<your-server-ip>:5000/api/users'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<dynamic> users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                var user = users[index];
+                return ListTile(
+                  title: Text(user['username']),
+                  subtitle: Text(user['email']),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+    );
   }
 }

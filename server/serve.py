a2e784bde6023a7ff2b5b70cd661762687fd2a15
amspsingh04 <sp.singh.150604@gmail.com>
2024-04-17 00:28:09 +0530

    .

--- server/serve.py
+++ server/serve.py
@@ -3,6 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 DATABASE = {
     'database': 'dbms_proj',
@@ -25,6 +26,96 @@ def get_students():
     cursor.close()
     conn.close()
     return jsonify(students)
+@app.route('/books', methods=['GET'])
+def get_books():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM book;')
+    books = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(books)
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM course;')
+    courses = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(courses)
+
+@app.route('/faculty', methods=['GET'])
+def get_faculty():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM faculty;')
+    faculty_members = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(faculty_members)
+
+@app.route('/menu', methods=['GET'])
+def get_menu():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM menu;')
+    menu_items = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(menu_items)
+
+@app.route('/mess', methods=['GET'])
+def get_mess():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM mess;')
+    mess_details = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(mess_details)
+
+@app.route('/hostels', methods=['GET'])
+def get_hostels():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM hostel;')
+    hostels = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(hostels)
+
+@app.route('/maintenance', methods=['GET'])
+def get_maintenance_requests():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM maintenance;')
+    maintenance_requests = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(maintenance_requests)
+
+@app.route('/borrow', methods=['GET'])
+def get_borrow():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM borrow;')
+    borrow_records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(borrow_records)
+
+@app.route('/enrollments', methods=['GET'])
+def get_enrollments():
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT * FROM enrollment;')
+    enrollments = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(enrollments)
 
 if __name__ == '__main__':
     app.run(debug=True)
+

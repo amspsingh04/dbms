f61ce528fa9b6c3ff71cce158693871ebfb00306
amspsingh04 <sp.singh.150604@gmail.com>
2024-04-17 02:43:59 +0530

    .

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -37,12 +37,46 @@ $(document).ready(function() {
         });
     }
 
-    // Event handler for the dropdown's change event
     $('#data-selector').change(function() {
         const selectedData = $(this).val();
         if (selectedData) {
+            // Show the Add Data button when a data type is selected
+            $('#add-data-button').show();
+
+            // Optionally: Load the data for the selected data type
             loadData('/' + selectedData);
+
+            // ... You can also load the form structure for the modal here, if needed ...
+        } else {
+            // Hide the Add Data button when no data type is selected
+            $('#add-data-button').hide();
         }
     });
-});
 
+    // Event handler for 'Add Data' button click
+    $('#add-data-button').click(function() {
+        // Show the modal window
+        $('#add-data-modal').show();
+
+        // Populate the modal with a form based on the selected data type
+        const selectedDataType = $('#data-selector').val();
+        if (selectedDataType) {
+            // An example of how you might start a request to get form fields
+            // $.getJSON('/get_form_fields/' + selectedDataType, function(fields) {
+            //     // Generate the form using the fields provided by the server
+            // });
+        }
+
+        // Rest of the logic to generate the form goes here
+    });
+
+    // Event handler for 'Submit Data' button click (within the modal form)
+    $('#submit-data-button').click(function() {
+        // Gather the form data and perform the form submission logic here
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    $('.close').click(function() {
+        $('#add-data-modal').hide();
+    });
+});

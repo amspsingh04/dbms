cad81045e764d549c8bbc978127de70e7642e05a
amspsingh04 <sp.singh.150604@gmail.com>
2024-04-19 19:19:58 +0530

    .

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Server Ping',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _serverResponse = 'Server response will be shown here';
+
+  Future<void> _pingServer() async {
+    final response = await http
+        .get(Uri.parse('http://<your-flask-server-ip>:<port>/some-endpoint'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _serverResponse = response.body;
+      });
+    } else {
+      setState(() {
+        _serverResponse = 'Failed to load response from server';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Server Ping'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Ping Server'),
+              onPressed: _pingServer,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(_serverResponse),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- project/test/widget_test.dart
+++ project/test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project/app.dart';
 
 import 'package:project/main.dart';
 
